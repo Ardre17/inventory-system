b47ef5e371dc944a14187236ecb512d15e09fffa
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-05-29 18:54:02 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 env > /app/.env
+echo "DB_CONNECTION=pgsql" >> /app/.env
 
+php artisan key:generate --force
 php artisan config:clear
 php artisan migrate:fresh --force
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
