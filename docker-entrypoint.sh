5d2b49405e747cf18cb588e1e37c95b495cf7166
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-05-29 17:04:55 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 set -e
 
-# Crear .env con variables de Railway
-printenv | grep -E "^(APP_|DB_|SESSION_|CACHE_|QUEUE_)" > /app/.env
-
-# Asegurar valores fijos
-echo "SESSION_DRIVER=file" >> /app/.env
-echo "CACHE_STORE=file" >> /app/.env
-echo "QUEUE_CONNECTION=sync" >> /app/.env
-echo "DB_CONNECTION=pgsql" >> /app/.env
+env > /app/.env
 
 php artisan config:clear
 php artisan migrate:fresh --force
