28b88fbb28fc738b884488366eaccd26826cb5e8
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-05-30 09:31:56 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 printf "APP_ENV=production\n" > /app/.env
-printf "APP_DEBUG=false\n" >> /app/.env
+printf "APP_DEBUG=true\n" >> /app/.env
 printf "APP_URL=https://inventory-system-production-a650.up.railway.app\n" >> /app/.env
 printf "DB_CONNECTION=pgsql\n" >> /app/.env
 printf "DB_HOST=%s\n" "$DB_HOST" >> /app/.env
@@ -14,6 +14,10 @@ printf "SESSION_DRIVER=file\n" >> /app/.env
 printf "CACHE_STORE=file\n" >> /app/.env
 printf "QUEUE_CONNECTION=sync\n" >> /app/.env
 printf "LOG_CHANNEL=stderr\n" >> /app/.env
+
+echo "=== CONTENIDO DEL .ENV ==="
+cat /app/.env
+echo "=========================="
 
 php artisan key:generate --force
 php artisan config:clear
